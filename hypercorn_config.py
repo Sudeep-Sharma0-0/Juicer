9b1a78c96468d1b1e29440ccf8cdd5e17de44f9c
Sudeep Sharma <sharma.sudip1122@gmail.com>
2024-04-23 20:42:46 +0545

    Test: Remove Past Dirs

--- hypercorn_config.py
+++
@@ -1,2 +0,0 @@
-bind = '127.0.0.1:8000'  # Replace with your desired IP and port
-workers = 4  # Number of worker processes/threads
--- splitter/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SplitterConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'splitter'
